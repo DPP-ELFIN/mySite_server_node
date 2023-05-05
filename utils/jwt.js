9acfcef98466f62844f93c860e86e87f96e1facf
zwp <1519037604@qq.com.com>
2023-05-05 09:53:58 +0800

    jsonwebtoken

---
+++ utils/jwt.js
@@ -0,0 +1,26 @@
+/** @format */
+
+const jwt = require("jsonwebtoken");
+
+const { promisify } = require("util");
+
+const sign = promisify(jwt.sign);
+const verify = promisify(jwt.verify);
+
+module.exports.createToken = async (userInfo) => {
+  return await sign({ userInfo }, "a76c7437-e36e-43b7-92f2-483e0ac53ed3", { expiresIn: 60 * 60 * 24 });
+};
+
+module.verifyToken = async (req, res, next) => {
+  const token = req.headers.token.split("Bearer ")[1];
+  if (!token) {
+    res.status(401).json({ error: "身份验证失败！" });
+  }
+  try {
+    let userinfo = await verify(token, "a76c7437-e36e-43b7-92f2-483e0ac53ed3");
+    req.user = userinfo;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "身份验证失败！" });
+  }
+};
