82024309374e36c882579e972aa013fea8f0fea6
DPP-ELFIN <1519037604@qq.com>
2023-05-16 20:23:56 +0800

    提交代码

--- model/index.js
+++ model/index.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/mySite");
+  await mongoose.connect("mongodb://127.0.0.1:27017/mySite");
 }
 
 main()
