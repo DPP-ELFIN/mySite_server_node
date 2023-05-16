2196a534059eb8418c9332755fed2f1d41d0e8ce
zwp <1519037604@qq.com.com>
2023-05-16 16:47:13 +0800

    项目管理-接口

--- controller/demandController.js
+++ controller/demandController.js
@@ -7,6 +7,26 @@ const { Demand } = require("../model/index");
  * @param {Request} req
  * @param {Response} res
  */
+
+module.exports.editStatus = async (req, res, next) => {
+  // console.log(req.body);
+  const dbb = await Demand.findOneAndUpdate({ _id: req.body.id }, req.body, { new: true });
+  res.msg = "修改成功";
+  (res.data = dbb), res.status(201);
+  next();
+};
+
+module.exports.delDemand = async (req, res, next) => {
+  const dbb = await Demand.findByIdAndDelete({ _id: req.query.id });
+  if (dbb) {
+    res.data = dbb;
+    res.msg = "删除成功";
+    res.status(200);
+  } else {
+    res.status(204);
+  }
+  next();
+};
 module.exports.getDemand = async (req, res, next) => {
   let dbb = await Demand.find(req.query);
   res.data = dbb;
@@ -16,7 +36,6 @@ module.exports.getDemand = async (req, res, next) => {
 };
 
 module.exports.addDemand = async (req, res, next) => {
-  console.log(req.body);
   const demand = new Demand(req.body);
   const dbb = await demand.save();
   res.data = dbb;
--- router/demand.js
+++ router/demand.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const { verifyToken } = require("../utils/jwt");
-const { addDemand, getDemand } = require("../controller/demandController");
+const { addDemand, getDemand, delDemand, editStatus } = require("../controller/demandController");
 const { addDemandvali } = require("../middleware/demandValidator");
 
 const router = express.Router();
 
 router.post("/addDemand", verifyToken, addDemandvali, addDemand);
+router.post("/editStatus", verifyToken, editStatus);
 router.get("/getDemand", verifyToken, getDemand);
+router.delete("/delDemand", verifyToken, delDemand);
 
 module.exports = router;
